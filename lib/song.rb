629a01e6a79a4ecf4b9451ec194c6ed9657d07c8
Adam Weissman <adam@comedytragedyepic.com>
2019-10-12 16:37:46 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,40 @@
+class Song
+
+  attr_accessor :name, :artist, :genre
+  def initialize(name, artist = nil, genre = nil)
+    @name = name
+    self.artist = artist if artist
+    self.genre = genre if genre
+  end
+    
+  @@all = []
+  
+  def self.all
+    @@all
+  end
+  
+  def self.destroy_all
+    @@all.clear
+  end
+  
+  def artist=(artist)
+    @artist = artist
+    artist.add_song(self)
+  end
+  
+  def self.create(name)
+    song = new(name)
+    song.save
+    song
+  end
+  
+  def save
+    @@all << self
+  end
+  
+  def genre=(genre)
+    @genre = genre
+    genre.songs.push self if not genre.songs.include?(self)
+  end
+
+end
