0103271aa0be88905d3de988abfb12ddceb55452
Adam Weissman <adam@comedytragedyepic.com>
2019-10-12 17:46:08 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -44,5 +44,15 @@ class Song
     @genre = genre
     genre.songs.push self if not genre.songs.include?(self)
   end
+  
+  def self.new_from_filename(filename)
+    parts = filename.split(" - ")
+    artist_name, song_name, genre_name = parts[0], parts[1], parts[2].gsub(".mp3", "")
+
+    artist = Artist.find_or_create_by_name(artist_name)
+    genre = Genre.find_or_create_by_name(genre_name)
+
+    new(song_name, artist, genre)
+  end
 
 end
