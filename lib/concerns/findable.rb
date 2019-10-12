6132e22ea1336c8214f21b4158d0052a6add4c54
Adam Weissman <adam@comedytragedyepic.com>
2019-10-12 17:26:15 +0000

    Automatically backed up by Learn

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -1,5 +1,11 @@
 module Concerns::ModuleName
 
-
+  def self.find_by_name(fname)
+    all.detect {|song| song.name == fname}
+  end
+  
+  def self.find_or_create_by_name(fname)
+    find_by_name(fname) || create(fname)
+  end
   
 end
