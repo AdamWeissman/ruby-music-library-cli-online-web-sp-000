e2a11838f8aa57ac9d30f6b07f994620299f01c5
Adam Weissman <adam@comedytragedyepic.com>
2019-10-12 17:33:16 +0000

    Automatically backed up by Learn

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -1,25 +1,15 @@
-module Concerns::Findable
+module Concerns
+  module Findable
+    def find_by_name(name)
+      self.all.detect {|object| object.name == name}
+    end
 
-  def find_by_name(fname)
-    self.all.detect {|song| song.name == fname}
-  end
-  
-  def find_or_create_by_name(fname)
-    self.find_by_name(fname) || self.create(fname)
-  end
-  
-end
-
-=begin
-
-module Concerns::Findable
-  def find_by_name(name)
-    self.all.detect{|s| s.name == name}
-  end
-
-  def find_or_create_by_name(name)
-    self.find_by_name(name) || self.create(name)
+    def find_or_create_by_name(name)
+      if self.find_by_name(name).nil?
+        self.create(name)
+      else
+        self.find_by_name(name)
+      end
+    end
   end
 end
-
-=end 
